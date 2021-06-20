3e1ea7f6dee9ee63f1e0017df10296b4feb52bd8
bknohut <bknohut@gmail.com>
2021-06-20 16:14:44 +0200

    Added BKBot

---
+++ BKBot.js
@@ -0,0 +1,99 @@
+// Berke Kagan Nohut
+// 27.02.2020 - v.1.0
+// 27.12.2020 - v.1.1
+// 20.06.2021 - v.1.2
+
+// imports
+const Discord = require('discord.js');
+const fs = require('fs');
+require('dotenv').config();
+
+// client definition
+const client = new Discord.Client();
+client.login(process.env.BOT_TOKEN);
+// constants
+const resources = './resources';
+const soundFilePath = resources + '/sounds/';
+
+// variables
+const commands = [];
+const commandMap = {};
+const commandQueue = [];
+let globalCommandQueueLock = false;
+
+// on bot launch
+client.on('ready', () => {
+	console.log('BKBot Status: Logged in as BKBot');
+	const files = fs.readdirSync(soundFilePath);
+	for(let i = 0; i < files.length; i++) {
+		const key = files[i].split('.').slice(0, -1).join('.');
+		commands.push(key);
+		commandMap[key] = soundFilePath + files[i];
+	}
+});
+
+
+// on member sent message
+client.on('message', async message => {
+	if(message.author.bot) return;
+
+	if(message.content.indexOf(process.env.PREFIX) !== 0) return;
+
+	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ !BK/g);
+	const command = args.shift().toLowerCase();
+	if(command === 'help') {
+		let m = 'BKBot Help\n' +
+            '!BK prefix to command the bot\n' +
+            'Available Commands:\n';
+		for(let i = 0; i < commands.length; i++) {
+			m = m + '!BK ' + commands[i] + '\n';
+		}
+		message.channel.send(m);
+		return;
+	}
+	for(let i = 0; i < commands.length; i++) {
+		if(command == commands[i]) {
+			const connection = await joinAuthorChannel(message.member);
+			playSound(connection, commandMap[command]);
+		}
+	}
+});
+
+/*
+// runs when a member joins a voice channel
+client.on('voiceStateUpdate', (oldmember, newmember)=>{
+    //let oldvoice = oldmember.voiceChannel;
+    let newvoice = newmember.voiceChannel;
+})
+*/
+
+async function joinAuthorChannel(author) {
+	const connection = await author.voice.channel.join();
+	return connection;
+}
+
+async function playSound(connection, sound) {
+
+	if(!globalCommandQueueLock) {
+		globalCommandQueueLock = true;
+		connection.play(sound).on('finish', () => {
+            console.log('Finished playing sound.');
+            console.log('The number of commands waiting to be executed is: ' + commandQueue.length);
+            globalCommandQueueLock = false;
+            if(commandQueue.length > 0) {
+                commandQueue.shift()();
+            }
+            else{
+                connection.disconnect();
+            }
+        }).on('error', () => {
+            console.log('Error occurred while executing command. The voice channel connection might have failed. Exitting...');
+            connection.disconnect();
+        });
+	}
+	else{
+		commandQueue.push(function() {playSound(connection, sound);});
+        console.log('Sound added to queue');
+        console.log(commandQueue.length + ' are waiting in line to be executed.');
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # BKBot
 A Discord Bot to play sounds.
+
+# Usage
+
+BKBot reads a "resource" folder in its path.
+Any sound file that is recognizable by ffmpeg v.0.0.4 can be played.
+
+It discovers the sound files on bot launch. Simply put all the sounds in ./resources/ before launching the bot.
+
+Use !BK help to see available commands.
+
+!BK <command> to run.
+
