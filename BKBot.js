84f72bea5b076cc88ed7132f44af258dc4fea48f
bknohut <bknohut@gmail.com>
2021-07-25 15:37:24 +0200

    Added TTS

--- BKBot.js
+++ BKBot.js
@@ -2,10 +2,12 @@
 // 27.02.2020 - v.1.0
 // 27.12.2020 - v.1.1
 // 20.06.2021 - v.1.2
+// 25.07.2021 - v.1.3
 
 // imports
 const Discord = require('discord.js');
 const fs = require('fs');
+const discordTTS = require('discord-tts');
 require('dotenv').config();
 
 // client definition
@@ -39,18 +41,44 @@ client.on('message', async message => {
 
 	if(message.content.indexOf(process.env.PREFIX) !== 0) return;
 
-	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ !BK/g);
-	const command = args.shift().toLowerCase();
-	if(command === 'help') {
+	const args = message.content.slice(process.env.PREFIX.length).trim().split(process.env.PREFIX);
+	const command = args.shift().toLowerCase().split(' ');
+
+	if(command.length == 1 && command[0] === 'help') {
 		let m = 'BKBot Help\n' +
-            '!BK prefix to command the bot\n' +
+            '!BK prefix to command me\n' +
             'Available Commands:\n';
 		for(let i = 0; i < commands.length; i++) {
 			m = m + '!BK ' + commands[i] + '\n';
 		}
+		m = m + '!BK say <Text> to make the me speak the words.';
 		message.channel.send(m);
 		return;
 	}
+
+	if(command.length > 1 && command[0] === 'say') {
+		if(globalCommandQueueLock) {
+			return;
+		}
+
+		globalCommandQueueLock = true;
+		const connection = await joinAuthorChannel(message.member);
+		const broadcast = client.voice.createBroadcast();
+		const duration = command.length * 500;
+		const text = command.slice(1).join(' ');
+		broadcast.play(discordTTS.getVoiceStream(text));
+		connection.play(broadcast);
+		setTimeout(() => {
+			globalCommandQueueLock = false;
+			if(commandQueue.length > 0) {
+                commandQueue.shift()();
+			}
+			else{
+				connection.disconnect();
+			}
+		}, 3000 + duration);
+	}
+
 	for(let i = 0; i < commands.length; i++) {
 		if(command == commands[i]) {
 			const connection = await joinAuthorChannel(message.member);
